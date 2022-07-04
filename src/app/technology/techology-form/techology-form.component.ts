3dc4753138a44c36f1e422e7d5a2a7c42c5e2b71
Mário Junior <48899365+Mario23junior@users.noreply.github.com>
2022-07-04 13:07:48 -0300

    aplicando o valor minimo

--- src/app/technology/techology-form/techology-form.component.ts
+++ src/app/technology/techology-form/techology-form.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { TechnologysService } from '../services/technologys.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-techology-form',
@@ -14,11 +15,13 @@ export class TechologyFormComponent implements OnInit {
   public form: FormGroup
   private service: TechnologysService
   private snackBar: MatSnackBar
+  private location: Location
 
 
-  constructor(formBuilder: FormBuilder, service: TechnologysService, snackBar: MatSnackBar) {
+  constructor(formBuilder: FormBuilder, service: TechnologysService, snackBar: MatSnackBar, location: Location) {
     this.formBuilder = formBuilder
     this.service = service
+    this.location = location
     this.snackBar = snackBar
     this.form = this.formBuilder.group({
       name: [null],
@@ -31,17 +34,23 @@ export class TechologyFormComponent implements OnInit {
 
   onSubmit() {
     this.service.save(this.form.value)
-      .subscribe(result => console.log(result),
+      .subscribe(result => this.onSuccess(),
         error => this.onError())
   }
 
   onCancel() {
-
+     this.location.back()
   }
 
   private onError() {
     this.snackBar.open('Erro ao salvar uma nova tecnologia,por favor tente mais tarde', '',
       { duration: 5000, panelClass: ['mat-toolbar', 'mat-warn'] })
+  }
+
+  private onSuccess() {
+    this.snackBar.open('Tecnlogia salva com sucesso !', '',
+      { duration: 5000, panelClass: ['mat-toolbar', 'mat-Accent'] })
+      this.onCancel()
   }
 
 }
